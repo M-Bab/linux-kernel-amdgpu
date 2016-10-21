6479f4a20c004fc42ec865ce2a88fd712625af0b
Harry Wentland <harry.wentland@amd.com>
2016-10-20 21:31:38 -0400

    drm/amd/dal: Move DDC reg def into hw_factory
    Change-Id: I192c38b380809b2a3b1352cf6b00f682c784760a
    Signed-off-by: Harry Wentland <harry.wentland@amd.com>
    Acked-by: Harry Wentland <harry.wentland@amd.com>

--- drivers/gpu/drm/amd/dal/dc/gpio/dce110/hw_factory_dce110.c
+++ drivers/gpu/drm/amd/dal/dc/gpio/dce110/hw_factory_dce110.c
@@ -33,13 +33,11 @@
 
 #include "hw_factory_dce110.h"
 
-#include "hw_ddc_dce110.h"
-
 #include "dce/dce_11_0_d.h"
 #include "dce/dce_11_0_sh_mask.h"
 
 /* set field name */
-#define SF(reg_name, field_name, post_fix)\
+#define SF_HPD(reg_name, field_name, post_fix)\
 	.field_name = reg_name ## __ ## field_name ## post_fix
 
 #include "reg_helper.h"
@@ -67,6 +65,66 @@ static const struct hpd_sh_mask hpd_mask = {
 		HPD_MASK_SH_LIST(_MASK)
 };
 
+#include "../ddc_regs.h"
+
+ /* set field name */
+#define SF_DDC(reg_name, field_name, post_fix)\
+	.field_name = reg_name ## __ ## field_name ## post_fix
+
+static const struct ddc_registers ddc_data_regs[] = {
+	ddc_data_regs(1),
+	ddc_data_regs(2),
+	ddc_data_regs(3),
+	ddc_data_regs(4),
+	ddc_data_regs(5),
+	ddc_data_regs(6),
+	ddc_vga_data_regs,
+	ddc_i2c_data_regs
+};
+
+static const struct ddc_registers ddc_clk_regs[] = {
+	ddc_clk_regs(1),
+	ddc_clk_regs(2),
+	ddc_clk_regs(3),
+	ddc_clk_regs(4),
+	ddc_clk_regs(5),
+	ddc_clk_regs(6),
+	ddc_vga_clk_regs,
+	ddc_i2c_clk_regs
+};
+
+static const struct ddc_sh_mask ddc_shift = {
+		DDC_MASK_SH_LIST(__SHIFT)
+};
+
+static const struct ddc_sh_mask ddc_mask = {
+		DDC_MASK_SH_LIST(_MASK)
+};
+
+static void define_ddc_registers(
+		struct hw_gpio_pin *pin,
+		uint32_t en)
+{
+	struct hw_ddc *ddc = HW_DDC_FROM_BASE(pin);
+
+	switch (pin->id) {
+	case GPIO_ID_DDC_DATA:
+		ddc->regs = &ddc_data_regs[en];
+		ddc->base.regs = &ddc_data_regs[en].gpio;
+		break;
+	case GPIO_ID_DDC_CLOCK:
+		ddc->regs = &ddc_clk_regs[en];
+		ddc->base.regs = &ddc_clk_regs[en].gpio;
+		break;
+	default:
+		ASSERT_CRITICAL(false);
+		return;
+	}
+
+	ddc->shifts = &ddc_shift;
+	ddc->masks = &ddc_mask;
+
+}
 
 static void define_hpd_registers(struct hw_gpio_pin *pin, uint32_t en)
 {
@@ -86,7 +144,7 @@ static const struct hw_factory_funcs funcs = {
 	.create_sync = NULL,
 	.create_gsl = NULL,
 	.define_hpd_registers = define_hpd_registers,
-	.define_ddc_registers = define_ddc_dce110_registers
+	.define_ddc_registers = define_ddc_registers
 };
 
 /*
--- drivers/gpu/drm/amd/dal/dc/gpio/dce80/hw_factory_dce80.c
+++ drivers/gpu/drm/amd/dal/dc/gpio/dce80/hw_factory_dce80.c
@@ -31,7 +31,6 @@
 
 #include "../hw_gpio.h"
 #include "../hw_ddc.h"
-#include "hw_ddc_dce80.h"
 #include "../hw_hpd.h"
 
 #include "dce/dce_8_0_d.h"
@@ -73,6 +72,67 @@ static const struct hpd_sh_mask hpd_mask = {
 		HPD_MASK_SH_LIST_DCE8(_MASK)
 };
 
+#include "../ddc_regs.h"
+
+ /* set field name */
+#define SF_DDC(reg_name, field_name, post_fix)\
+	.field_name = reg_name ## __ ## field_name ## post_fix
+
+static const struct ddc_registers ddc_data_regs[] = {
+	ddc_data_regs(1),
+	ddc_data_regs(2),
+	ddc_data_regs(3),
+	ddc_data_regs(4),
+	ddc_data_regs(5),
+	ddc_data_regs(6),
+	ddc_vga_data_regs,
+	ddc_i2c_data_regs
+};
+
+static const struct ddc_registers ddc_clk_regs[] = {
+	ddc_clk_regs(1),
+	ddc_clk_regs(2),
+	ddc_clk_regs(3),
+	ddc_clk_regs(4),
+	ddc_clk_regs(5),
+	ddc_clk_regs(6),
+	ddc_vga_clk_regs,
+	ddc_i2c_clk_regs
+};
+
+static const struct ddc_sh_mask ddc_shift = {
+		DDC_MASK_SH_LIST(__SHIFT)
+};
+
+static const struct ddc_sh_mask ddc_mask = {
+		DDC_MASK_SH_LIST(_MASK)
+};
+
+static void define_ddc_registers(
+		struct hw_gpio_pin *pin,
+		uint32_t en)
+{
+	struct hw_ddc *ddc = HW_DDC_FROM_BASE(pin);
+
+	switch (pin->id) {
+	case GPIO_ID_DDC_DATA:
+		ddc->regs = &ddc_data_regs[en];
+		ddc->base.regs = &ddc_data_regs[en].gpio;
+		break;
+	case GPIO_ID_DDC_CLOCK:
+		ddc->regs = &ddc_clk_regs[en];
+		ddc->base.regs = &ddc_clk_regs[en].gpio;
+		break;
+	default:
+		ASSERT_CRITICAL(false);
+		return;
+	}
+
+	ddc->shifts = &ddc_shift;
+	ddc->masks = &ddc_mask;
+
+}
+
 static void define_hpd_registers(struct hw_gpio_pin *pin, uint32_t en)
 {
 	struct hw_hpd *hpd = HW_HPD_FROM_BASE(pin);
@@ -91,7 +151,7 @@ static const struct hw_factory_funcs funcs = {
 	.create_sync = NULL,
 	.create_gsl = NULL,
 	.define_hpd_registers = define_hpd_registers,
-	.define_ddc_registers = define_ddc_dce80_registers
+	.define_ddc_registers = define_ddc_registers
 };
 
 void dal_hw_factory_dce80_init(
