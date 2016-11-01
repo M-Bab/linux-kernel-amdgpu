950252280682b8d1c57e3bc4ea265ffbcf0eb0a4
Harry Wentland <harry.wentland@amd.com>
2016-11-01 16:20:55 -0400

    drm/amd/dal: Make set_overscan_blank_color optional
    Change-Id: I14a34f81c063d31f064bfc7ad6efcaef5b60f7fd
    Signed-off-by: Harry Wentland <harry.wentland@amd.com>
    Acked-by: Harry Wentland <harry.wentland@amd.com>

--- drivers/gpu/drm/amd/dal/dc/dce110/dce110_hw_sequencer.c
+++ drivers/gpu/drm/amd/dal/dc/dce110/dce110_hw_sequencer.c
@@ -756,9 +756,10 @@ static void program_scaler(const struct core_dc *dc,
 		pipe_ctx->scl_data.lb_bpp,
 		&pipe_ctx->stream->bit_depth_params);
 
-	pipe_ctx->tg->funcs->set_overscan_blank_color(
-		pipe_ctx->tg,
-		&color);
+	if (pipe_ctx->tg->funcs->set_overscan_blank_color)
+		pipe_ctx->tg->funcs->set_overscan_blank_color(
+				pipe_ctx->tg,
+				&color);
 
 	pipe_ctx->xfm->funcs->transform_set_scaler(pipe_ctx->xfm,
 		&pipe_ctx->scl_data);
