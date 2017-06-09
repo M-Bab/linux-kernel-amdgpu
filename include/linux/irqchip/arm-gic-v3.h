08153c996ec5d4d9d4627740b3e89c5795911621
Marc Zyngier <marc.zyngier@arm.com>
2017-06-09 12:49:37 +0100

    KVM: arm64: vgic-v3: Add ICV_EOIR1_EL1 handler
    BugLink: https://bugs.launchpad.net/bugs/1673564
    
    Add a handler for writing the guest's view of the ICC_EOIR1_EL1
    register. This involves dropping the priority of the interrupt,
    and deactivating it if required (EOImode == 0).
    
    Tested-by: Alexander Graf <agraf@suse.de>
    Acked-by: David Daney <david.daney@cavium.com>
    Reviewed-by: Eric Auger <eric.auger@redhat.com>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    Reviewed-by: Christoffer Dall <cdall@linaro.org>
    Signed-off-by: Christoffer Dall <cdall@linaro.org>
    (backported from commit b6f49035b4bf6e2709f2a5fed3107f5438c1fd02)
    [ dannf: Dropped SYS_ prefix on ICV_EOIR1_EL1 macro, which wasn't
      added upstream until 0e9884fe ]
    Signed-off-by: dann frazier <dann.frazier@canonical.com>
    
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>

--- include/linux/irqchip/arm-gic-v3.h
+++ include/linux/irqchip/arm-gic-v3.h
@@ -403,6 +403,8 @@
 
 #define ICH_HCR_EN			(1 << 0)
 #define ICH_HCR_UIE			(1 << 1)
+#define ICH_HCR_EOIcount_SHIFT		27
+#define ICH_HCR_EOIcount_MASK		(0x1f << ICH_HCR_EOIcount_SHIFT)
 
 #define ICH_VMCR_CBPR_SHIFT		4
 #define ICH_VMCR_CBPR_MASK		(1 << ICH_VMCR_CBPR_SHIFT)
