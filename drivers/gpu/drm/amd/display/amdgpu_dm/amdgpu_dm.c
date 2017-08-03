b8fb3e71f9cd71ac330507c85ad1d727df3f0f2f
Alex Deucher <alexander.deucher@amd.com>
2017-08-03 14:44:25 -0400

    drm/amd/display: fix up drm_accurate_vblank_count rename
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -229,7 +229,7 @@ static void dm_pflip_high_irq(void *interrupt_params)
 	/* wakeup usersapce */
 	if (amdgpu_crtc->event) {
 		/* Update to correct count/ts if racing with vblank irq */
-		drm_accurate_vblank_count(&amdgpu_crtc->base);
+		drm_crtc_accurate_vblank_count(&amdgpu_crtc->base);
 
 		drm_crtc_send_vblank_event(&amdgpu_crtc->base, amdgpu_crtc->event);
 
