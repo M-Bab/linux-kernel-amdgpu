4a0756ef3e754acc98883c70886c068bdfa8e215
Jerry (Fangzhi) Zuo <Jerry.Zuo@amd.com>
2018-04-17 13:49:48 -0400

    drm/amd/display: Update MST edid property every time
    Extended fix to: "Don't read EDID in atomic_check"
    
    Fix display property not observed in GUI display after hot plug.
    
    Call drm_mode_connector_update_edid_property every time in
    .get_modes hook, due to the fact that edid property is getting
    removed from usermode ioctl DRM_IOCTL_MODE_GETCONNECTOR each time
    in hot unplug.
    
    Signed-off-by: Jerry (Fangzhi) Zuo <Jerry.Zuo@amd.com>
    Reviewed-by: Harry Wentland <Harry.Wentland@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_mst_types.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_mst_types.c
@@ -254,10 +254,10 @@ static int dm_dp_mst_get_modes(struct drm_connector *connector)
 		if (aconnector->dc_sink)
 			amdgpu_dm_update_freesync_caps(
 					connector, edid);
-
-		drm_mode_connector_update_edid_property(
-						&aconnector->base, edid);
 	}
+
+	drm_mode_connector_update_edid_property(
+					&aconnector->base, aconnector->edid);
 
 	ret = drm_add_edid_modes(connector, aconnector->edid);
 
