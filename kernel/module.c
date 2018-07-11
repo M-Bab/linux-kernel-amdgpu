d0c140cef7b58cffa86ec27d83ea3568722586a5
Seth Forshee <seth.forshee@canonical.com>
2018-07-11 08:35:56 -0500

    Revert "UBUNTU: SAUCE: (efi-lockdown) lockdown: fix coordination of kernel module signature verification"
    This reverts commit c5790abd088e78e0f5c9a9d868941f3b08ab706c.
    The implementation of this feature is flawed and causes modules
    to fail to load under kernel lockdown unless the modules have
    signatures for IMA appraisal.
    
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>

--- kernel/module.c
+++ kernel/module.c
@@ -2801,7 +2801,9 @@ static int module_sig_check(struct load_info *info, int flags,
 		if (is_module_sig_enforced()) {
 			pr_notice("%s is rejected\n", reason);
 			return -EKEYREJECTED;
-		} else if (can_do_ima_check && is_ima_appraise_enabled())
+		}
+
+		if (can_do_ima_check && is_ima_appraise_enabled())
 			return 0;
 		if (kernel_is_locked_down(reason))
 			return -EPERM;
