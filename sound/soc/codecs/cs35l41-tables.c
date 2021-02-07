eecdf4e5b36bf97486d49357741d8189d664ca0b
David Rhodes <david.rhodes@cirrus.com>
2021-02-06 19:53:10 -0600

    ASoC: cs35l41: Add Hibernate Registers to tables
    Add hibernation-specific registers to the regmap
    readable and volatile functions.
    
    Signed-off-by: David Rhodes <david.rhodes@cirrus.com>

--- sound/soc/codecs/cs35l41-tables.c
+++ sound/soc/codecs/cs35l41-tables.c
@@ -151,6 +151,9 @@ bool cs35l41_readable_reg(struct device *dev, unsigned int reg)
 	case CS35L41_PROTECT_REL_ERR_IGN:
 	case CS35L41_GPIO_PAD_CONTROL:
 	case CS35L41_JTAG_CONTROL:
+	case CS35L41_PWRMGT_CTL:
+	case CS35L41_WAKESRC_CTL:
+	case CS35L41_PWRMGT_STS:
 	case CS35L41_PLL_CLK_CTRL:
 	case CS35L41_DSP_CLK_CTRL:
 	case CS35L41_GLOBAL_CLK_CTRL:
@@ -620,6 +623,7 @@ bool cs35l41_volatile_reg(struct device *dev, unsigned int reg)
 	case CS35L41_FABID:
 	case CS35L41_REVID:
 	case CS35L41_DTEMP_EN:
+	case CS35L41_PWRMGT_STS:
 	case CS35L41_IRQ1_STATUS:
 	case CS35L41_IRQ1_STATUS1:
 	case CS35L41_IRQ1_STATUS2:
